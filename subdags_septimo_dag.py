92ce666e30d886cf6faaea8dcd814b97bdd7d57f
Tinmarian <tinmar96@gmail.com>
2023-02-13 14:12:45 -0600

    Final commit

--- subdags_septimo_dag.py
+++ subdags_septimo_dag.py
@@ -13,11 +13,11 @@ default_args = {
 def hello():
     print('Hola Mundo')
 
-def load_subdag(parent_dag_name,child_dag_name,default_args):
+def load_subdag(parent_dag_name,task_id,default_args):
     with DAG(
-            child_dag_name,
+            dag_id=f'{parent_dag_name}.{task_id}',
             default_args=default_args,
-            schedule_interval=None
+            schedule_interval='@daily'
         ) as subdag:
 
         sub_start = DummyOperator(task_id='sub_start')
@@ -29,23 +29,29 @@ def load_subdag(parent_dag_name,child_dag_name,default_args):
 
         sub_end = DummyOperator(task_id='sub_end')
 
-        sub_start >> python_task >>sub_end
+        sub_start >> python_task >> sub_end
 
     return subdag
 
 with DAG(
         'subdags_septimo_dag',
         catchup=False,
-        schedule_interval=None,
+        schedule_interval='@daily',
         default_args=default_args,
         tags=['Curso 2', 'Apache_Airflow']
     ) as dag:
 
     start = DummyOperator(task_id='start')
 
+    subdag_args = {
+        "parent_dag_name":"subdags_septimo_dag",
+        "task_id":"subdag",
+        "default_args":default_args
+    }
+
     subdag = SubDagOperator(
                             task_id='subdag',
-                            subdag=load_subdag   
+                            subdag=load_subdag(**subdag_args) 
                         )
 
     end = DummyOperator(task_id='end')
