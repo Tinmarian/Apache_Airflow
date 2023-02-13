ac005396a1c8bce4f1f839ada535ac7c04519d2f
Tinmarian <tinmar96@gmail.com>
2023-02-13 17:58:13 -0600

    Dynamic Creation of DAG's

---
+++ dynamic_dags_decimo_dag.py
@@ -0,0 +1,43 @@
+
+from airflow.models import DAG
+from datetime import datetime
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+
+default_args = {
+
+}
+
+def create_dag(dag_id,schedule,default_args,pais):
+
+    dag = DAG(
+                dag_id,  # 'dynamic_dags_decimo_dag',
+                catchup=False,
+                schedule_interval=schedule,
+                default_args=default_args,
+                tags=['Curso 2', 'Apache_Airflow']
+            )
+
+    with dag:
+
+        start = DummyOperator(task_id='start')
+
+        imprimir_pais = BashOperator(
+                                        task_id=f'imprimir_{pais}',
+                                        bash_command=f'echo {pais}'
+                                    )
+
+        end = DummyOperator(task_id='end')
+
+        start >> imprimir_pais >> end
+
+    return dag
+
+paises = ['Mexico','Brasil','Nicaragua']
+
+for pais in paises:
+    dag_id=f"dynamic.dags.decimo.dag.{pais}"
+    default_args={"owner":f"Tinmar_{pais}","start_date":datetime(2023,1,13)}
+    
+    globals()[dag_id] = create_dag(dag_id,None,default_args=default_args,pais=pais)
